27e98b4b7508b34f24fd5655c5df61aede46a8ed
MSI\Nico <lepilolodior7633@gmail.com>
2019-06-24 14:11:11 +0200

    14.11

---
+++ Assets/Scripts/Highlighted_Card_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighted_Card_Script : MonoBehaviour
+{
+    Animation animHover;
+	// Use this for initialization
+	void Start ()
+    {
+        animHover = GetComponent<Animation>();
+    }
+
+    public void playAnimHover()
+    {
+        animHover.Play();
+    }
+
+    public void stopAnimHover()
+    {
+        animHover.Stop();
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
